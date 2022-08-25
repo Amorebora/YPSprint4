c6f66a0032bfd865f4a87712d1ee1efc259da68e
Maksim Baev <maximbaeves@gmail.com>
2022-08-25 22:02:32 +0200

    last3
    last3

---
+++ MovieQuiz/Helpers/UI color.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    // swiftlint:disable force_unwrapping
+    static var black: UIColor { UIColor(named: "black")! }
+    static var darkGray: UIColor { UIColor(named: "darkGray")! }
+    static var red: UIColor { UIColor(named: "red")! }
+    static var gray: UIColor { UIColor(named: "gray")! }
+    static var green: UIColor { UIColor(named: "green")! }
+    static var white: UIColor { UIColor(named: "white")! }
+// swiftlint: enable force_unwrapping
+}
